9db8672b4a69689e36491542f289a7e0f1a78133
Danko Dudar <danko.dudar@gmail.conm>
2025-02-10 16:45:41 +0200

    Add more code (Finalization)

--- lesson4-arrays-and-objects/src/loop.js
+++ lesson4-arrays-and-objects/src/loop.js
@@ -1,7 +1,28 @@
-for (let a = 0; a < 10; a++) {
-    console.log(a);
-};
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
 
-for (let a = 100; a >= 0; a -= 10) {
-    console.log(a);
-};
+for (let i = 100; i >= 0; i -= 10) {
+    console.log(i);
+}
+
+let j = 0;
+while (j < 10) {
+    console.log(j);
+    j++;
+}
+
+do {
+    console.log(j);
+    j--;
+} while (j > 0);
+
+const array = ['a', 'b', 'c', 'd'];
+for (const value of array) {
+    console.log(value);
+}
+
+const obj = { a: 1, b: 2, c: 3 };
+for (const key in obj) {
+    console.log(`${key}: ${obj[key]}`);
+}
